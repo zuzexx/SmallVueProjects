56308d32b52c32c35aafbaa58d9136b5c093b35a
zuzexx <tjasa.zilavec@gmail.com>
2022-09-01 20:24:55 +0200

    .made hero

--- simple-bulma-page/src/components/TopComponent/TopComponent.vue
+++ simple-bulma-page/src/components/TopComponent/TopComponent.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>TopComponent</div>
+  <section class="hero is-fullheight has-background-black">
+    <div class="hero-body">
+      <div class="container">
+        <figure class="image center">
+          <img
+            src="@/assets/bulma-logo.png"
+            alt="bulma logo"
+            style="max-width: 512px"
+          />
+        </figure>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   name: "TopComponent",
 };
 </script>
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
