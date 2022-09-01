58bac32675372bb9d17cc04423f7d6a58ca16141
zuzexx <tjasa.zilavec@gmail.com>
2022-09-01 21:03:33 +0200

    .middle containers styled to completion

--- simple-bulma-page/src/components/MiddleComponent/MiddleComponent.vue
+++ simple-bulma-page/src/components/MiddleComponent/MiddleComponent.vue
@@ -2,35 +2,56 @@
   <section class="section has-background-light">
     <div class="container">
       <div class="columns">
-        <div class="notification is-primary margin">
-          <div class="column">
-            <h1 class="title is-size-5">Bulma?</h1>
-            <p class="is-size-6">
-              Bulma is a modern CSS framework from @jgthms, based on flexbox.
-              Using Bulma, we can describe our website's design using just
-              classes.
-            </p>
-          </div>
+        <div class="column">
+          <article class="media notification is-primary">
+            <figure class="media-left">
+              <img src="@/assets/code-language.png" alt="icon" />
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <h1 class="title is-size-5">Bulma?</h1>
+                <p class="is-size-6">
+                  Bulma is a modern CSS framework from @jgthms, based on
+                  flexbox. Using Bulma, we can describe our website's design
+                  using just classes.
+                </p>
+              </div>
+            </div>
+          </article>
         </div>
-        <div class="notification is-info margin">
-          <div class="column">
-            <h1 class="title is-size-5">...Flexbox?</h1>
-            <p class="is-size-6">
-              Flexbox is a CSS spec that makes sectioning and aligning more
-              natural. We do not need to know flexbox but it's how Bulma works
-              behind-the-scenes.
-            </p>
-          </div>
+        <div class="column">
+          <article class="media notification is-info">
+            <figure class="media-left">
+              <img src="@/assets/code.png" alt="icon" />
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <h1 class="title is-size-5">...Flexbox?</h1>
+                <p class="is-size-6">
+                  Flexbox is a CSS spec that makes sectioning and aligning more
+                  natural. We do not need to know flexbox but it's how Bulma
+                  works behind-the-scenes.
+                </p>
+              </div>
+            </div>
+          </article>
         </div>
-        <div class="notification is-warning margin">
-          <div class="column">
-            <h1 class="title is-size-5">...Classes?</h1>
-            <p class="is-size-6">
-              Instead of writing our CSS per-element, we can use predefined
-              classes. With enough classes, we can describe our website design
-              using semantics.
-            </p>
-          </div>
+        <div class="column">
+          <article class="media notification is-warning">
+            <figure class="media-left">
+              <img src="@/assets/coffee-mug.png" alt="icon" />
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <h1 class="title is-size-5">...Classes?</h1>
+                <p class="is-size-6">
+                  Instead of writing our CSS per-element, we can use predefined
+                  classes. With enough classes, we can describe our website
+                  design using semantics.
+                </p>
+              </div>
+            </div>
+          </article>
         </div>
       </div>
     </div>
@@ -44,5 +65,9 @@ export default {
 <style scoped>
 .margin {
   margin: 10px;
+}
+img {
+  width: 27px;
+  height: auto;
 }
 </style>
