bfc32dcaab19ad6611d32f30cf4e94328fd4cd6d
zuzexx <tjasa.zilavec@gmail.com>
2022-09-01 21:30:34 +0200

    .bottom component and readme done

--- simple-bulma-page/README.md
+++ simple-bulma-page/README.md
@@ -2,10 +2,10 @@
 
 ## About:
 
-This is a simple page made with Buefy and Vue3.
+This is a simple page made with Buefy and Vue3. Icons are sourced from [Flaticon](https://www.flaticon.com/). I helped myself with a [freeCodeCamp tutorial](https://www.youtube.com/watch?v=LBzZLzu2GKo&ab_channel=freeCodeCamp.org).
 
 ## Screenshots:
 
-![mobile](/simple-bulma-page/src/assets/bulma-logo.png)
-![tablet]()
-![desktop]()
+![mobile](/simple-bulma-page/src/assets/mobile.JPG)
+
+![desktop](/simple-bulma-page/src/assets/desktop.JPG)
--- simple-bulma-page/src/components/BottomComponent/BottomComponent.vue
+++ simple-bulma-page/src/components/BottomComponent/BottomComponent.vue
@@ -1,8 +1,73 @@
 <template>
-  <div>bottom component</div>
+  <section>
+    <div class="container">
+      <div class="columns is-multiline">
+        <div v-for="text in texts" :key="text" class="column is-one-third">
+          <article class="media notification has-background-white">
+            <figure class="media-left">
+              <img :src="text.image" alt="icon" />
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <h1 class="title is-size-5">{{ text.title }}</h1>
+                <p class="subtitle is-size-6">
+                  {{ text.paragraph }}
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   name: "BottomComponent",
+  data() {
+    return {
+      texts: [
+        {
+          title: "Columns",
+          paragraph: "The power of flexbox in a simple interface",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147718.png",
+        },
+        {
+          title: "Form",
+          paragraph:
+            "The indispensable form controls, designed for maximum clarity",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147794.png",
+        },
+        {
+          title: "Components",
+          paragraph:
+            "Advanced multi-part components with lots of possibilities",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147767.png",
+        },
+        {
+          title: "Modifiers",
+          paragraph: "An easy-to-read naming system designed for humans",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147779.png",
+        },
+        {
+          title: "Layout",
+          paragraph:
+            "Design the structure of your webpage with these CSS classes",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147771.png",
+        },
+        {
+          title: "Elements",
+          paragraph:
+            "Essential interface elements that only require a single CSS class",
+          image: "https://cdn-icons-png.flaticon.com/512/2147/2147719.png",
+        },
+      ],
+    };
+  },
 };
 </script>
+<style scoped>
+img {
+  width: 20px;
+}
+</style>
